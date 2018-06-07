2f2808ef88256418b690235f1bc9f4c01dca9067
James Seppi <james.seppi@mapbox.com>
2018-06-07 10:32:26 -0500

    fix flow

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,3 +1,4 @@
+/* @flow */
 import React from 'react';
 import Helmet from 'react-helmet';
 
@@ -5,18 +6,14 @@ import Helmet from 'react-helmet';
 // ("window not defined" error during `npm run build`), 
 // so used the unpkg-hosted version instead
 
-export default class Admin extends React.Component {
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <link
-            href="https://unpkg.com/netlify-cms@^1.0.0/dist/cms.css"
-            rel="stylesheet"
-          />
-          <script src="https://unpkg.com/netlify-cms@^1.0.0/dist/cms.js"></script>
-        </Helmet>
-      </div>
-    );
-  }
-}
+export default () => (
+  <div>
+    <Helmet>
+      <link
+        href="https://unpkg.com/netlify-cms@^1.0.0/dist/cms.css"
+        rel="stylesheet"
+      />
+      <script src="https://unpkg.com/netlify-cms@^1.0.0/dist/cms.js"></script>
+    </Helmet>
+  </div>
+);
