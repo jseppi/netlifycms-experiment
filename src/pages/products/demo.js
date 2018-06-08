acc455441d13b12e985dcf7c9d1d9a4128a18bc6
James Seppi <james.seppi@mapbox.com>
2018-06-08 16:11:17 -0500

    attempt react component type

---
+++ src/pages/products/demo.js
@@ -0,0 +1,17 @@
+/*---
+title: Demo Product
+subTitle: The demoiest product of them all.
+---*/
+
+export default class ProductDemo extends React.Component {
+  render() {
+    const { frontMatter } = this.props;
+    return (
+      <div>
+        <h1>{frontMatter.title}</h1>
+        <h2>{frontMatter.subTitle}</h2>
+        <p>Lorem ipsum blah blah blah</p>
+      </div>
+    );
+  }
+}
